2958348b6735f5d3ba5536d3fca9f39a887b306e
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2025-10-29 21:22:50 -0400

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+import store from './store';
 
 function App() {
   
